cd61298e0c80df699b4503c3836b6326c27f67cc
Noorkhokhar <34125851+noorkhokhar99@users.noreply.github.com>
2020-11-18 12:24:46 +0500

    Add files via upload

---
+++ basic_demo.ino
@@ -0,0 +1,89 @@
+#include "Seeed_HM330X.h"
+
+#ifdef  ARDUINO_SAMD_VARIANT_COMPLIANCE
+    #define SERIAL_OUTPUT SerialUSB
+#else
+    #define SERIAL_OUTPUT Serial
+#endif
+
+HM330X sensor;
+uint8_t buf[30];
+
+
+const char* str[] = {"sensor num: ", "PM1.0 concentration(CF=1,Standard particulate matter,unit:ug/m3): ",
+                     "PM2.5 concentration(CF=1,Standard particulate matter,unit:ug/m3): ",
+                     "PM10 concentration(CF=1,Standard particulate matter,unit:ug/m3): ",
+                     "PM1.0 concentration(Atmospheric environment,unit:ug/m3): ",
+                     "PM2.5 concentration(Atmospheric environment,unit:ug/m3): ",
+                     "PM10 concentration(Atmospheric environment,unit:ug/m3): ",
+                    };
+
+HM330XErrorCode print_result(const char* str, uint16_t value) {
+    if (NULL == str) {
+        return ERROR_PARAM;
+    }
+    SERIAL_OUTPUT.print(str);
+    SERIAL_OUTPUT.println(value);
+    return NO_ERROR;
+}
+
+/*parse buf with 29 uint8_t-data*/
+HM330XErrorCode parse_result(uint8_t* data) {
+    uint16_t value = 0;
+    if (NULL == data) {
+        return ERROR_PARAM;
+    }
+    for (int i = 1; i < 8; i++) {
+        value = (uint16_t) data[i * 2] << 8 | data[i * 2 + 1];
+        print_result(str[i - 1], value);
+
+    }
+
+    return NO_ERROR;
+}
+
+HM330XErrorCode parse_result_value(uint8_t* data) {
+    if (NULL == data) {
+        return ERROR_PARAM;
+    }
+    for (int i = 0; i < 28; i++) {
+        SERIAL_OUTPUT.print(data[i], HEX);
+        SERIAL_OUTPUT.print("  ");
+        if ((0 == (i) % 5) || (0 == i)) {
+            SERIAL_OUTPUT.println("");
+        }
+    }
+    uint8_t sum = 0;
+    for (int i = 0; i < 28; i++) {
+        sum += data[i];
+    }
+    if (sum != data[28]) {
+        SERIAL_OUTPUT.println("wrong checkSum!!!!");
+    }
+    SERIAL_OUTPUT.println("");
+    return NO_ERROR;
+}
+
+
+/*30s*/
+void setup() {
+    SERIAL_OUTPUT.begin(115200);
+    delay(100);
+    SERIAL_OUTPUT.println("Serial start");
+    if (sensor.init()) {
+        SERIAL_OUTPUT.println("HM330X init failed!!!");
+        while (1);
+    }
+
+}
+
+
+void loop() {
+    if (sensor.read_sensor_value(buf, 29)) {
+        SERIAL_OUTPUT.println("HM330X read result failed!!!");
+    }
+    parse_result_value(buf);
+    parse_result(buf);
+    SERIAL_OUTPUT.println("");
+    delay(5000);
+}
